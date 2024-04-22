6f628d0bc0ed387fa6e7890484c5e991577154a5
LuChengChiu <lucheng.chiu0501@gmail.com>
2024-04-22 16:43:59 +0800

    url debug

--- src/component/api/axios.js
+++ src/component/api/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+const LOCAL_URL = "http://localhost:5600";
+const ONLINE_URL = "https://titaniumserver.zeabur.app";
+
 export default axios.create({
-  baseURL: `http://localhost:5600`,
+  baseURL: ONLINE_URL,
 });
